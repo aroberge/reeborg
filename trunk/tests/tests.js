468ea338af28cb64fb7aab103920acd25a1528d4
andre.roberge@gmail.com <andre.roberge@gmail.com@ec9b96ea-facd-7dc9-34b9-829c92dc2a88>
2013-01-02 21:29:00 +0000

    Robot can move and turn in an empty world.

--- trunk/tests/tests.js
+++ trunk/tests/tests.js
@@ -1,24 +1,48 @@
+module("World");
+
+test("Creation", 4, function(){
+    // WORLD created by default
+    deepEqual(WORLD.robots, {}, "World starts with no robots");
+    strictEqual(WORLD.robot_counter, 0, "World starts with no robots");
+    strictEqual(WORLD.needs_update, true, "World starts needing update - for drawing");
+
+    WORLD.update();
+    strictEqual(WORLD.needs_update, false, "World has been updated"); 
+});
+
+test("Adding robot", 3, function(){
+    var reeborg = new UsedRobot();
+    strictEqual(WORLD.needs_update, true, "The world needs to be updated");
+    deepEqual(WORLD.robots, {"robot0": reeborg}, "World has one more robot");
+    strictEqual(WORLD.robot_counter, 1, "World has one more robot");
+});
+
 module('UsedRobot', {
     setup: function(){
-        UNIVERSE.reset();
+        WORLD.reset();
         ok(true, "module setup run")
     }
 });
 
-test( "Creation", 15, function() {
+test( "Creation", 20, function() {
     var reeborg = new UsedRobot();
     strictEqual(reeborg.x, 1, "reeborg default x coordinate");
     strictEqual(reeborg.y, 1, "reeborg default y coordinate");
     strictEqual(reeborg.orientation, 0, "reeborg default orientation");
     strictEqual(reeborg.coins, 0, "reeborg default coins");
     strictEqual(reeborg.changed, true, "reeborg has been changed upon creation");
-    strictEqual(UNIVERSE.needs_update, true, "The universe needs to be updated");
+    strictEqual(WORLD.needs_update, true, "The world needs to be updated");
+    deepEqual(WORLD.robots, {"robot0": reeborg}, "World has one more robot");
+    strictEqual(WORLD.robot_counter, 1, "World has one more robot");
 
-    reeborg = new UsedRobot(2, 3, "N", 2);
-    strictEqual(reeborg.x, 2, "reeborg assigned x coordinate");
-    strictEqual(reeborg.y, 3, "reeborg assigned y coordinate");
-    strictEqual(reeborg.orientation, 1, "reeborg assigned orientation");
-    strictEqual(reeborg.coins, 2, "reeborg assigned coins");
+    var reeborg2 = new UsedRobot(2, 3, "N", 2);
+    strictEqual(reeborg2.x, 2, "reeborg assigned x coordinate");
+    strictEqual(reeborg2.y, 3, "reeborg assigned y coordinate");
+    strictEqual(reeborg2.orientation, 1, "reeborg assigned orientation");
+    strictEqual(reeborg2.coins, 2, "reeborg assigned coins");
+    strictEqual(WORLD.needs_update, true, "The world needs to be updated");
+    deepEqual(WORLD.robots, {"robot0": reeborg, "robot1":reeborg2}, "World has two robots");
+    strictEqual(WORLD.robot_counter, 2, "World has two robots");
 
     reeborg = new UsedRobot(1, 1, "west");
     strictEqual(reeborg.orientation, 2, "reeborg assigned orientation");
@@ -55,5 +79,24 @@ test( "Turning - both languages", 7, function() {
     strictEqual(reeborg.orientation, 2, "reeborg new orientation");
     reeborg.tourne_à_gauche();
     strictEqual(reeborg.orientation, 3, "reeborg new orientation");
+});
+
+test("Moving in empty world - both languages", 7, function(){
+    var reeborg = new UsedRobot();
+    var reeborg2 = new UsedRobot();
+    reeborg.move();
+    strictEqual(reeborg.x, 2, "reeborg new position");
+    reeborg2.move();
+    strictEqual(reeborg2.x, 2, "reeborg2 new position");    
+    reeborg.turn_left();
+    reeborg.move();
+    strictEqual(reeborg.y, 2, "reeborg new position");
+    reeborg.turn_left()
+    reeborg.avance();
+    strictEqual(reeborg.x, 1, "reeborg new position");
+    raises(function () {
+        reeborg.move();
+    }, "Hit wall exception", "Must throw error to pass.");
+    strictEqual(reeborg.x, 1, "reeborg unchanged position");
 });
 
