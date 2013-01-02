ee404f0858873b84130378981079e496af428963
andre.roberge@gmail.com <andre.roberge@gmail.com@ec9b96ea-facd-7dc9-34b9-829c92dc2a88>
2013-01-02 21:29:00 +0000

    Robot can move and turn in an empty world.

--- src/js/reeborg.js
+++ src/js/reeborg.js
@@ -1,8 +1,13 @@
-function Universe () {
+function World () {
+    this.EAST = 0;
+    this.NORTH = 1;
+    this.WEST = 2;
+    this.SOUTH = 3;
+
     this.reset = function (){
         this.robots = {};
         this.robot_counter = 0;
-        this.needs_update = false;
+        this.needs_update = true;   // true indicates that we should redraw
     }
     this.reset();
     this.add_robot = function (robot) {
@@ -10,8 +15,43 @@ function Universe () {
         this.robot_counter++;
         this.needs_update = true;
     }
+
+    this.update = function(){
+        this.needs_update = false;
+    }
+
+    this.move_robot = function(robot){
+        switch (robot.orientation){
+            case this.EAST:
+                robot.x += 1;
+                break; 
+            case this.NORTH:
+                robot.y += 1;
+                break;
+            case this.WEST:
+                if (robot.x==1){
+                    throw "Hit wall exception";
+                } else {
+                    robot.x -= 1;
+                }
+                break;
+            case this.SOUTH:
+                if (robot.y==1){
+                    throw "Hit wall exception";
+                } else {
+                    robot.y -= 1;
+                }
+                break;      
+            default:
+                console.log("Should not happen: unhandled case in World.move_robot().")
+                console.log("robot.x= ", robot.x, " robot.y= ", robot.y, "robot.orientation= ", robot.orientation);
+                throw "Should not happen: unhandled case in World.move_robot().";
+                break;
+        }
+    }
 }
-UNIVERSE = new Universe();
+
+WORLD = new World();
 
 function UsedRobot(x, y, orientation, coins) {
     this.x = x || 1;
@@ -19,35 +59,36 @@ function UsedRobot(x, y, orientation, coins) {
     this.coins = coins || 0;
     this["pièces"] = this.coins;    // pièces de monnaie == coins
     this.changed = true;
-    UNIVERSE.add_robot(this);
+    WORLD.add_robot(this);
 
     if (orientation === undefined){
-        this.orientation = 0;
+        this.orientation = WORLD.EAST;
     } else {
         switch (orientation.toLowerCase()){
             case "e":
             case "east":
             case "est":
-                this.orientation = 0;
+                this.orientation = WORLD.EAST;
                 break;
             case "n":
             case "north":
             case "nord":
-                this.orientation = 1;
+                this.orientation = WORLD.NORTH;
                 break;
             case "w":
             case "o":
             case "west":
             case "ouest":
-                this.orientation = 2;
+                this.orientation = WORLD.WEST;
                 break;
             case "s":
             case "south":
             case "sud":
-                this.orientation = 3;
+                this.orientation = WORLD.SOUTH;
                 break;
             default:
-                this.orientation = 0;
+                throw "Should not happen: unknown orientation";
+                // TODO: turn this into a warning
         }
     }
 }
@@ -60,4 +101,11 @@ UsedRobot.prototype.turn_left = function(){
 };
 
 UsedRobot.prototype["tourne_à_gauche"] = UsedRobot.prototype.turn_left;
+
+UsedRobot.prototype.move = function(){
+    WORLD.move_robot(this);
+}
+
+UsedRobot.prototype.avance = UsedRobot.prototype.move;
+
 
