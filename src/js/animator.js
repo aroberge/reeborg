d05512ac9c337f3d3d2407ec7a6835a208fdc52a
andre.roberge@gmail.com <andre.roberge@gmail.com@ec9b96ea-facd-7dc9-34b9-829c92dc2a88>
2013-01-22 03:32:06 +0000

    Basic trace (robot leak) implemented.

--- src/js/animator.js
+++ src/js/animator.js
@@ -11,6 +11,7 @@ RUR.visible_world = {
         this.height = this.background_canvas.height;
         this.width = this.background_canvas.width;
         this.wall_ctx = document.getElementById("wall_canvas").getContext("2d");
+        this.trace_ctx = document.getElementById("trace_canvas").getContext("2d");
         this.robot_ctx = document.getElementById("robot_canvas").getContext("2d");
         this.rows = Math.floor(this.height / this.wall_length) - 2;
         this.cols = Math.floor(this.width / this.wall_length) - 2;
@@ -26,7 +27,6 @@ RUR.visible_world = {
         this.robot_w_img.src = '../src/images/robot_w.png';
         this.robot_s_img = new Image();
         this.robot_s_img.src = '../src/images/robot_s.png';
-        this.draw_background_walls();
         this.draw();
         this.running = false;
     },
@@ -37,6 +37,9 @@ RUR.visible_world = {
     wall_color: "brown",
     shawdow_wall_color: "#f0f0f0",
     ctx: null,
+    trace_color: "seagreen",
+    trace_offset : [[30, 30], [30, 20], [20, 20], [20, 30]],
+    //trace_offset : [[25, 30], [25, 25], [25, 20], [25, 25]],
     draw : function () {
         "use strict";
         // fake wall configuration
@@ -55,6 +58,7 @@ RUR.visible_world = {
         "use strict";
         var i, j;
         this.ctx = this.background_ctx;
+        this.ctx.clearRect(0, 0, RUR.visible_world.width, RUR.visible_world.height);
         this.ctx.fillStyle = this.shawdow_wall_color;
         for (i = 1; i <= this.cols; i++) {
             for (j = 1; j <= this.rows; j++) {
@@ -67,6 +71,7 @@ RUR.visible_world = {
         "use strict";
         var key, i, j;
         this.ctx = this.wall_ctx;
+        this.ctx.clearRect(0, 0, RUR.visible_world.width, RUR.visible_world.height);
         this.ctx.fillStyle = this.wall_color;
         for (i = 0; i <= this.cols; i++) {
             for (j = 0; j <= this.rows; j++) {
@@ -124,6 +129,18 @@ RUR.visible_world = {
             img = RUR.visible_world.robot_e_img;
         }
         this.ctx.drawImage(img, x, y);
+        this.draw_trace(robot);
+    },
+    draw_trace : function (robot) {
+        "use strict";
+        this.ctx = this.trace_ctx;
+        this.ctx.strokeStyle = this.trace_color;
+        this.ctx.beginPath();
+        this.ctx.moveTo(robot.prev_x* this.wall_length + RUR.visible_world.trace_offset[robot.prev_orientation][0],
+                        this.height - (robot.prev_y +1) * this.wall_length + RUR.visible_world.trace_offset[robot.prev_orientation][1]);
+        this.ctx.lineTo(robot.x* this.wall_length + RUR.visible_world.trace_offset[robot.orientation][0],
+                        this.height - (robot.y +1) * this.wall_length + RUR.visible_world.trace_offset[robot.orientation][1]);
+        this.ctx.stroke();
     },
     play_frames : function () {
         "use strict";
@@ -141,7 +158,7 @@ RUR.visible_world = {
         }
         RUR.visible_world.draw_frames();
         if (RUR.world.frames.length !== 0) {
-            setTimeout(RUR.visible_world.update, 500);
+            setTimeout(RUR.visible_world.update, 50);
         }
     },
     draw_frames : function () {
@@ -160,6 +177,9 @@ RUR.visible_world = {
         "use strict";
         var dummy;
         RUR.world.reset();
+        this.draw_background_walls();
+        this.draw_foreground_walls();
+        this.trace_ctx.clearRect(0, 0, RUR.visible_world.width, RUR.visible_world.height);
         dummy = new UsedRobot();
         this.draw_frames();
         RUR.visible_world.running = false;
