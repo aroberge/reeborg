260b02e8fab07122a296c52adf1c6a4ce2f9b4af
andre.roberge@gmail.com <andre.roberge@gmail.com@ec9b96ea-facd-7dc9-34b9-829c92dc2a88>
2013-02-01 19:19:55 +0000

    resizing ui should now work properly...

--- trunk/tests/jsbin_look.js
+++ trunk/tests/jsbin_look.js
@@ -1,7 +1,7 @@
 /*jshint browser:true, devel:true */
 /*globals $ */
 
-var MAX_WIDTH, MIN_WIDTH = 100;
+var MAX_WIDTH, MIN_WIDTH = 200;
 
 function reset_widths () {
     var all_active_panels, children, index, child;
@@ -18,27 +18,12 @@ function reset_widths () {
         // WHY do I need to subtract 8 ??    2 x padding + 1 for border perhaps?...
         all_active_panels[index].width($("#panels").width()/all_active_panels.length - 8);
     }
-    for (index = 0; index < all_active_panels.length - 1; index++) {
+    for (index = 0; index < all_active_panels.length; index++) {
         set_resizable(all_active_panels, index);
     }
     return all_active_panels;
 }
 
-function available_width(resizing_panel, all_active_panels) {
-    var index, this_panel, next_panel;
-    var remaining = 0;
-    next_panel = undefined;
-    for (index = 0; index < all_active_panels.length; index++) {
-        this_panel = all_active_panels[index];
-        if (resizing_panel.attr("id") === this_panel.attr("id")) {
-            remaining += this_panel.width();
-            next_panel = all_active_panels[index+1];
-        } else if (this_panel === next_panel){
-            remaining += next_panel.width();
-            return remaining;
-        }
-    }
-}
 
 function set_resizable(all_active_panels, index){
     var this_panel, next_panel;
@@ -48,19 +33,12 @@ function set_resizable(all_active_panels, index){
         handles: 'e',
         maxWidth: MAX_WIDTH,
         minWidth: MIN_WIDTH,
-        resize: function(event, ui){
-            var current_width = ui.size.width;
-            var remaining, max_width;
-            max_width = $(this).resizable( "option", "maxWidth");
-            remaining = available_width($(this), all_active_panels);
-            if (remaining < max_width + MIN_WIDTH) {
-                $(this).resizable( "option", "maxWidth", Math.max(remaining - MIN_WIDTH, MIN_WIDTH));
-            }
-            $(this).width(current_width);
-            next_panel.width(remaining - current_width);
+        helper: "resizable-helper",
+        stop: function(event, ui){
+            var remaining = next_panel.width() - (ui.size.width - ui.originalSize.width);
+            next_panel.width(remaining);
         }
     });
-
 }
 
 
