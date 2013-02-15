e696fa85e4adf472bdceabbebd5b8bd6a1398fc0
andre.roberge@gmail.com <andre.roberge@gmail.com@ec9b96ea-facd-7dc9-34b9-829c92dc2a88>
2013-02-15 18:54:22 +0000

    Separated out the French language stuff (there was surprisingly little); had to remove orientation information without being able to easily put it in a French module.  I did this so that, when I implement the French version, I can be (more) assured that it will be a good model for how to adapt to other languages if needed.

---
+++ src/js/reeborg_fr.js
@@ -0,0 +1,9 @@
+RUR = RUR || {};
+
+RUR.PrivateRobot.prototype.a_une_fuite = RUR.PrivateRobot.prototype.is_leaky;
+RUR.PrivateRobot.prototype.tourne_à_gauche = RUR.PrivateRobot.prototype.turn_left;
+RUR.PrivateRobot.prototype.avance = RUR.PrivateRobot.prototype.move;
+var avance = move;
+var tourne_à_gauche = turn_left;
+var pense = think;
+var RobotUsagé = UsedRobot;
