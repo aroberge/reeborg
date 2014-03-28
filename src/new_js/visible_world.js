7df10c5695ef77f3ef2822ef3f2ef5b8d1f8d96a
andre.roberge@gmail.com <andre.roberge@gmail.com@ec9b96ea-facd-7dc9-34b9-829c92dc2a88>
2014-03-28 01:52:37 +0000

    Can now draw everything.

--- src/new_js/visible_world.js
+++ src/new_js/visible_world.js
@@ -84,9 +84,7 @@ RUR.__visible_world.draw_foreground_walls = function (walls) {
             RUR.__visible_world.draw_east_wall(ctx, i, j);
         }
     }
-
 };
-
 
 RUR.__visible_world.draw_north_wall = function(ctx, i, j, goal) {
     "use strict";
@@ -178,10 +176,153 @@ RUR.__visible_world.draw_token = function (i, j, num, goal) {
     }
 };
 
+RUR.__visible_world.draw_goal = function () {
+    "use strict";
+    var goal, key, keys, i, j, k, ctx = RUR.__background_ctx;
+    if (RUR.__current_world.goal === undefined) {
+        return;
+    }
+
+    goal = RUR.__current_world.goal;
+    if (goal.position !== undefined) {
+        RUR.__visible_world.draw_coloured_tile(goal.position.x, goal.position.y, goal.orientation);
+    }
+    if (goal.shapes !== undefined){
+        RUR.__visible_world.draw_shapes(goal.shapes, true);
+    }
+    if (goal.tokens !== undefined) {
+        RUR.__visible_world.draw_tokens(goal.tokens, true);
+    }
+    if (goal.walls !== undefined){
+//        RUR.__visible_world.draw_foreground_walls(ctx, goal.walls, goal);
+        ctx.fillStyle = RUR.__wall_color;
+        keys = Object.keys(goal.walls);
+        for (key=0; key < keys.length; key++){
+            k = keys[key].split(",");
+            i = parseInt(k[0], 10);
+            j = parseInt(k[1], 10);
+            if ( goal.walls[keys[key]].indexOf("north") !== -1) {
+                RUR.__visible_world.draw_north_wall(ctx, i, j, true);
+            }
+            if (goal.walls[keys[key]].indexOf("east") !== -1) {
+                RUR.__visible_world.draw_east_wall(ctx, i, j, true);
+            }
+        }
+    }
+};
+
+
+RUR.__visible_world.draw_coloured_tile = function (i, j, orientation) {
+    var size = RUR.__wall_thickness, ctx = RUR.__background_ctx;
+    ctx.fillStyle = "#99ffcc";
+    ctx.fillRect(i*RUR.__wall_length + size, RUR.__height - (j+1)*RUR.__wall_length + size,
+                      RUR.__wall_length - size, RUR.__wall_length - size);
+    if (orientation === undefined) return;
+
+    ctx.fillStyle = "black";
+    switch(orientation){
+    case 0:
+        ctx.fillRect((i+1)*RUR.__wall_length - size, RUR.__height - (j+0.5)*RUR.__wall_length,
+                      size, size);
+        break;
+    case 1:
+        ctx.fillRect((i+0.5)*RUR.__wall_length, RUR.__height - (j+1)*RUR.__wall_length + size,
+                      size, size);
+        break;
+    case 2:
+        ctx.fillRect((i)*RUR.__wall_length + size, RUR.__height - (j+0.5)*RUR.__wall_length,
+                      size, size);
+        break;
+    case 3:
+        ctx.fillRect((i+0.5)*RUR.__wall_length , RUR.__height - (j)*RUR.__wall_length - size,
+                      size, size);
+        break;
+    }
+};
+
+RUR.__visible_world.draw_shapes = function(shapes, goal) {
+    "use strict";
+    var i, j, k, t, sh;
+    if (shapes === undefined) {
+        return;
+    }
+    sh = Object.keys(shapes);
+    for (t=0; t < sh.length; t++){
+        k = sh[t].split(",");
+        i = parseInt(k[0], 10);
+        j = parseInt(k[1], 10);
+        RUR.__visible_world.draw_shape(i, j, shapes[sh[t]], goal);
+    }
+};
+
+RUR.__visible_world.draw_shape = function (i, j, shape, goal) {
+    "use strict";
+    var ctx, size = 12, scale = RUR.__wall_length, Y = RUR.__height;
+    if(goal !== undefined){
+        ctx = RUR.__background_ctx;
+        ctx.lineWidth = 3;
+    } else {
+        ctx = RUR.__wall_ctx;
+    }
+    ctx.strokeStyle = "black";
+    if (shape === "square") {
+        ctx.fillStyle = "blue";
+        if(goal !== undefined){
+            ctx.beginPath();
+            ctx.rect((i+0.6)*scale - size, Y - (j+0.4)*scale - size, 2*size, 2*size);
+            ctx.stroke();
+        } else {
+            ctx.fillRect((i+0.6)*scale - size, Y - (j+0.4)*scale - size, 2*size, 2*size);
+        }
+    } else if (shape === "triangle") { // triangle
+        ctx.fillStyle = "green";
+        ctx.beginPath();
+        ctx.moveTo((i+0.6)*scale - size, Y - (j+0.4)*scale + size);
+        ctx.lineTo((i+0.6)*scale, Y - (j+0.4)*scale - size);
+        ctx.lineTo((i+0.6)*scale + size, Y - (j+0.4)*scale + size);
+        ctx.lineTo((i+0.6)*scale - size, Y - (j+0.4)*scale + size);
+        if(goal !== undefined) {
+            ctx.closePath();
+            ctx.stroke();
+        } else {
+            ctx.fill();
+        }
+    } else {
+        ctx.fillStyle = "red";
+        RUR.__visible_world.draw_star(ctx, (i+0.6)*scale, Y-(j+0.4)*scale, 1.5*size, goal);
+    }
+};
+
+RUR.__visible_world.draw_star = function (ctx, x, y, r, goal){
+    // adapted from https://developer.mozilla.org/en-US/docs/HTML/Canvas/Tutorial/Compositing
+    ctx.save();
+    ctx.translate(x, y);
+    ctx.save();
+    ctx.beginPath();
+    ctx.moveTo(r,0);
+    for (var i=0; i<9; i++){
+        ctx.rotate(Math.PI/5);
+        if(i%2 === 0) {
+            ctx.lineTo((r/0.525731)*0.200811, 0);
+        } else {
+            ctx.lineTo(r, 0);
+        }
+    }
+    ctx.closePath();
+    if (goal !== undefined){
+        ctx.lineWidth = 3;
+        ctx.stroke();
+    } else {
+        ctx.fill();
+    }
+    ctx.restore();
+    ctx.restore();
+};
 
 RUR.__visible_world.draw_all = function () {
     "use strict";
     RUR.__visible_world.draw_background();
+    RUR.__visible_world.draw_goal();
     RUR.__visible_world.refresh();
 };
 
@@ -190,4 +331,5 @@ RUR.__visible_world.refresh = function (world) {
     RUR.__visible_world.draw_foreground_walls(RUR.__current_world.walls);
     RUR.__visible_world.draw_robots(RUR.__current_world.robots);
     RUR.__visible_world.draw_tokens(RUR.__current_world.tokens);
+    RUR.__visible_world.draw_shapes(RUR.__current_world.shapes);
 };
