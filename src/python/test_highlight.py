8ca3561c11098ba194ecba39843eb933d1ed4d69
andre.roberge@gmail.com <andre.roberge@gmail.com@ec9b96ea-facd-7dc9-34b9-829c92dc2a88>
2014-11-05 05:35:37 +0000

    New tests cases added to highlighting

--- src/python/test_highlight.py
+++ src/python/test_highlight.py
@@ -1,29 +1,15 @@
 import unittest
 import highlight
 
-
-# simple
-simple_code_txt = """
+simple = """
 a=5
 for x in range(a):
     print(x)
 print("Happy end")
 """
 
-# if elif else
-ifelifelse_code_txt = """
-a=5
-if a > 8:
-    print("good)
-elif a >= 5:
-    print("not bad")
-else:
-    print("bad")
-print("Happy end")
-"""
-
 # result simple
-simple_result_txt = """
+simple_result = """
 _tp(1)
 a=5
 _tp(2)
@@ -34,33 +20,122 @@ for x in range(a):
 _tp(4)
 print("Happy end")
 """
-
-# big
-big_code_txt = """
-def fun(x):
-    s=x*2
-    print(x)
-    return(s)
-
-a=5
-stuff = []
+###################################
+# simple no spaces: no blank line at beginning and no \n at the end
+simple_1 = """a=5
 for x in range(a):
-    print("loop")
-    if x%2==0:
-        stuff.append(x)
-    else:
-        fun(x)
-print("Happy end")
-print( stuff )
+    print(x)
+print("Happy end")"""
+
+# result simple
+simple_result_1 = """_tp(0)
+a=5
+_tp(1)
+for x in range(a):
+    _tp(2)
+    print(x)
+    _tp(1)
+_tp(3)
+print("Happy end")"""
+
+#####################################
+# example of definition
+def_code = """
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
 """
+def_code_result = """
+_tp(1)
+def turn_right():
+    _tp(2)
+    turn_left()
+    _tp(3)
+    turn_left()
+    _tp(4)
+    turn_left()
+
+"""
+
+#####################################
+
+while_loop = """
+while token_here():
+    take()
+move()
+"""
+
+while_loop_result = """
+_tp(1)
+while token_here():
+    _tp(2)
+    take()
+    _tp(1)
+_tp(3)
+move()
+"""
+
+##########################################
+
+if_elif_else = """
+if right_is_clear():
+    turn_right()
+    move()
+elif front_is_clear():
+    move()
+else:
+    turn_left()
+"""
+
+if_elif_else_result = """
+_tp(1)
+if right_is_clear():
+    _tp(2)
+    turn_right()
+    _tp(3)
+    move()
+elif front_is_clear():
+    _tp(4)
+    RUR.control.null()
+    _tp(5)
+    move()
+else:
+    _tp(6)
+    RUR.control.null()
+    _tp(7)
+    turn_left()
+"""
+
+
 
 class TestSequenceFunctions(unittest.TestCase):
 
     def test_simple(self):
-        test = highlight.InsertTracer(simple_code_txt)
-        result = test.process()
-        self.assertEqual(simple_result_txt, result)
+        tracer = highlight.InsertTracer(simple)
+        result = tracer.process()
+        self.assertEqual(simple_result, result)
 
+    def test_simple_no_spaces(self):
+        tracer = highlight.InsertTracer(simple_1)
+        result = tracer.process()
+        self.assertEqual(simple_result_1, result)
+
+    def test_def(self):
+        tracer = highlight.InsertTracer(def_code)
+        result = tracer.process()
+        self.assertEqual(def_code_result, result)
+
+    def test_while(self):
+        tracer = highlight.InsertTracer(while_loop)
+        result = tracer.process()
+        self.assertEqual(while_loop_result, result)
+
+    def test_if(self):
+        tracer = highlight.InsertTracer(if_elif_else)
+        result = tracer.process()
+        self.assertEqual(if_elif_else_result, result)
 
 if __name__ == '__main__':
     unittest.main()
