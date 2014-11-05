1a0c14e28c48347d007abb7e03d1169819fd535a
andre.roberge@gmail.com <andre.roberge@gmail.com@ec9b96ea-facd-7dc9-34b9-829c92dc2a88>
2014-11-05 12:37:48 +0000

    No more failing tests.

--- trunk/src/python/test_highlight.py
+++ trunk/src/python/test_highlight.py
@@ -1,205 +1,43 @@
 import unittest
 import highlight
-
-simple = """
-a=5
-for x in range(a):
-    print(x)
-print("Happy end")
-"""
-
-# result simple
-simple_result = """
-_tp(1)
-a=5
-_tp(2)
-for x in range(a):
-    _tp(3)
-    print(x)
-    _tp(2)
-_tp(4)
-print("Happy end")
-"""
-###################################
-# simple no spaces: no blank line at beginning and no \n at the end
-simple_1 = """a=5
-for x in range(a):
-    print(x)
-print("Happy end")"""
-
-# result simple
-simple_result_1 = """_tp(0)
-a=5
-_tp(1)
-for x in range(a):
-    _tp(2)
-    print(x)
-    _tp(1)
-_tp(3)
-print("Happy end")"""
-
-#####################################
-# example of definition
-def_code = """
-def turn_right():
-    turn_left()
-    turn_left()
-    turn_left()
-
-"""
-def_code_result = """
-_tp(1)
-def turn_right():
-    _tp(2)
-    turn_left()
-    _tp(3)
-    turn_left()
-    _tp(4)
-    turn_left()
-
-"""
-
-#####################################
-
-while_loop = """
-while token_here():
-    take()
-move()
-"""
-
-while_loop_result = """
-_tp(1)
-while token_here():
-    _tp(2)
-    take()
-    _tp(1)
-_tp(3)
-move()
-"""
-
-#####################################
-
-only_while_loop = """
-while token_here():
-    take()
-"""
-
-only_while_loop_result = """
-_tp(1)
-while token_here():
-    _tp(2)
-    take()
-    _tp(1)
-"""
-
-##########################################
-
-if_elif_else = """
-if right_is_clear():
-    turn_right()
-    move()
-elif front_is_clear():
-    move()
-else:
-    turn_left()
-"""
-
-if_elif_else_result = """
-_tp(1)
-if right_is_clear():
-    _tp(2)
-    turn_right()
-    _tp(3)
-    move()
-elif front_is_clear():
-    _tp(4)
-    RUR.control.placeholder_frame()
-    _tp(5)
-    move()
-else:
-    _tp(6)
-    RUR.control.placeholder_frame()
-    _tp(7)
-    turn_left()
-"""
-
-###########################
-
-complex_code = """
-def follow_wall():
-    if right_is_clear():
-        turn_right()
-        move()
-    elif front_is_clear():
-        move()
-    else:
-        turn_left()
-while not at_goal():
-    follow_wall()
-"""
-complex_code_result = """
-_tp(1)
-def follow_wall():
-    _tp(2)
-    if right_is_clear():
-        _tp(3)
-        turn_right()
-        _tp(4)
-        move()
-    elif front_is_clear():
-        _tp(5)
-        RUR.control.placeholder_frame()
-        _tp(6)
-        move()
-    else:
-        _tp(7)
-        RUR.control.placeholder_frame()
-        _tp(8)
-        turn_left()
-_tp(9)
-while not at_goal():
-    _tp(10)
-    follow_wall()
-    _tp(9)
-"""
-
+import test_sources as src
 
 class TestSequenceFunctions(unittest.TestCase):
 
     def test_simple(self):
-        tracer = highlight.InsertTracer(simple)
+        tracer = highlight.InsertTracer(src.simple)
         result = tracer.process()
-        self.assertEqual(simple_result, result)
+        self.assertEqual(src.simple_result, result)
 
     def test_simple_no_spaces(self):
-        tracer = highlight.InsertTracer(simple_1)
+        tracer = highlight.InsertTracer(src.simple_1)
         result = tracer.process()
-        self.assertEqual(simple_result_1, result)
+        self.assertEqual(src.simple_result_1, result)
 
     def test_def(self):
-        tracer = highlight.InsertTracer(def_code)
+        tracer = highlight.InsertTracer(src.def_code)
         result = tracer.process()
-        self.assertEqual(def_code_result, result)
+        self.assertEqual(src.def_code_result, result)
 
     def test_while(self):
-        tracer = highlight.InsertTracer(while_loop)
+        tracer = highlight.InsertTracer(src.while_loop)
         result = tracer.process()
-        self.assertEqual(while_loop_result, result)
+        self.assertEqual(src.while_loop_result, result)
 
     def test_only_while(self):
-        tracer = highlight.InsertTracer(only_while_loop)
+        tracer = highlight.InsertTracer(src.only_while_loop)
         result = tracer.process()
-        self.assertEqual(only_while_loop_result, result)
+        self.assertEqual(src.only_while_loop_result, result)
 
     def test_if(self):
-        tracer = highlight.InsertTracer(if_elif_else)
+        tracer = highlight.InsertTracer(src.if_elif_else)
         result = tracer.process()
-        self.assertEqual(if_elif_else_result, result)
+        self.assertEqual(src.if_elif_else_result, result)
 
     def test_complex(self):
-        tracer = highlight.InsertTracer(complex_code)
+        tracer = highlight.InsertTracer(src.complex_code)
         result = tracer.process()
-        self.assertEqual(complex_code_result, result)
+        self.assertEqual(src.complex_code_result, result)
 
 if __name__ == '__main__':
     unittest.main()
