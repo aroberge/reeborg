9cc7dfcbe25d3e8a8c4ca59bf15ddd1de4638f30
andre.roberge@gmail.com <andre.roberge@gmail.com@ec9b96ea-facd-7dc9-34b9-829c92dc2a88>
2014-11-05 12:37:48 +0000

    No more failing tests.

--- src/python/highlight.py
+++ src/python/highlight.py
@@ -13,6 +13,8 @@
    and highlighted.
    '''
 
+DEBUG = False
+
 def rchop_by_set(mystr, separators):
     """ Splits a string into 2 parts without using regexp
         and return the first part (before a known separator)
@@ -20,6 +22,7 @@ def rchop_by_set(mystr, separators):
     for char in mystr:
         if char in separators:
             return mystr.split(char)[0]
+    return mystr   # e.g.  for "pass" or "continue" or "break"
 
 def repr_tracepoint( tc ):
     '''Determines how to represent trace point function'''
@@ -41,6 +44,7 @@ class InsertTracer():
     def __init__(self, src):
         src = src.replace('\t', '    ')  # standard tab spacing - just in case they are used
         self.lines = src.split("\n")
+        self.lines.append('pass')  # append a fake last null instruction to conclude indentation
         self.indentation_stack = [ dict(cause='', cause_lineno=0, indent='') ]
         self.trace_calls = []  # dict( when='before|after', place_lineno=, target_lineno=.., indent=.. )
 
@@ -55,7 +59,7 @@ class InsertTracer():
 
             line_wo_indent = line.lstrip()
             indent = line[:-len(line_wo_indent)]
-            first_word = rchop_by_set(line, ' =([{:\'"\\')  ############ possibly add # to see if first character - if so, simply return as comment
+            first_word = rchop_by_set(line_wo_indent, ' =([{:\'"\\')  ############ possibly add # to see if first character - if so, simply return as comment
 
             if self.indentation_stack[-1]['indent'] == None:  # if it was'nt set/known
                 self.indentation_stack[-1]['indent'] = indent
@@ -116,7 +120,7 @@ class InsertTracer():
                 )
 
         result = []
-        for lineno, line in enumerate(self.lines):
+        for lineno, line in enumerate(self.lines[:-1]):  # do not include last fake instruction
             # before
             if lineno in restructured_trace_calls:
                 for tc in restructured_trace_calls[lineno]['before']:
@@ -131,7 +135,24 @@ class InsertTracer():
                 for tc in restructured_trace_calls[lineno]['after-null']:
                     result.append(repr_tracepoint(tc))
                     result.append(repr_static(tc))
+
+        if DEBUG:
+            return '\n'.join(result), self.lines, restructured_trace_calls, self.trace_calls
         return '\n'.join(result)
 
 
+if __name__ == '__main__':
+    import test_sources as src
+    DEBUG = True
+
+    tracer = InsertTracer(src.complex_code)
+    result, lines, restructured_trace_calls, trace_calls = tracer.process()
+    for lineno, line in enumerate(lines[:-1]):
+        print(lineno, line)
+        print('   ', trace_calls[lineno])
+    print(src.complex_code_result)
+    for key in restructured_trace_calls:
+        for key2 in restructured_trace_calls[key]:
+            if restructured_trace_calls[key][key2]:
+                print(key, key2, restructured_trace_calls[key][key2])
 
