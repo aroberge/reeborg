555f8de770acf6cafbef50cb67a5b6211735a56a
andre.roberge@gmail.com <andre.roberge@gmail.com@ec9b96ea-facd-7dc9-34b9-829c92dc2a88>
2014-11-06 20:48:24 +0000

    Using a completely different approach to insert highlighting information.

--- src/python/highlight.py
+++ src/python/highlight.py
@@ -1,165 +1,72 @@
-# original code from https://code.google.com/p/pseudo-pdb/
-# by Jurgis Pralgauskis
+# Inspired from https://code.google.com/p/pseudo-pdb/ by Jurgis Pralgauskis
+# However, bearing very little resemblance to the above
+#
 # MIT license like the rest of Reeborg's World code.
-# Adapted by André Roberge
+# André Roberge
 
-''' Draft explanation of what this module is about:
 
-   This module contains functions that take a code sample and eventually
-   insert function calls before each line of code; these function calls
-   have the original line number of the line of code to be executed
-   (with some exceptions) allowing for source code in an editor to
-   have the line of code about to be executed properly identified
-   and highlighted.
-   '''
-
-DEBUG = False
-DEBUG_add_lineno = False
-
-def rchop_by_set(mystr, separators):
+def extract_first_word(mystr, separators):
     """ Splits a string into 2 parts without using regexp
-        and return the first part (before a known separator)
+        and return the first part (before a known word separator)
     """
-    for char in mystr:
+    for i, char in enumerate(mystr):
         if char in separators:
-            return mystr.split(char)[0]
-    return mystr   # e.g.  for "pass" or "continue" or "break"
+            return mystr[:i], mystr[i:]
+    return mystr, ''
 
-def repr_tracepoint( tc ):
-    '''Determines how to represent trace point function'''
-    tracecall_name = '_tp'
-    return tc['indent'] + tracecall_name +  '(%d)'%tc['target_lineno']
 
-def repr_static(tc):
-    '''Determines how to represent a static frame.
-       This will be used to "pause" the highlighting while Reeborg is
-       supposed to perform a clause like an "elif condition"
-    '''
-    return tc['indent'] + 'RUR.control.placeholder_frame()'
+def tracing_line(indent, lineno, frame=False):
+    '''Construct the tracing line'''
+    tracecall_name = 'RUR.set_lineno_highlight'
+    if frame:
+        return indent + tracecall_name +  '(%d, frame=True)'%lineno
+    else:
+        return indent + tracecall_name +  '(%d)'%lineno
 
-########################################################################
-#               The Code parsing and so on
-########################################################################
 
-class InsertTracer():
-    def __init__(self, src):
-        src = src.replace('\t', '    ')  # standard tab spacing - just in case they are used
-        self.lines = src.split("\n")
-        self.lines.append('pass')  # append a fake last null instruction to conclude indentation
-        self.indentation_stack = [ dict(cause='', cause_lineno=0, indent='') ]
-        self.trace_calls = []  # dict( when='before|after', place_lineno=, target_lineno=.., indent=.. )
+def insert_highlight_info(src):
+    src = src.replace('\t', '    ')  # standard tab spacing - just in case they are used
+    lines = src.split("\n")
+    new_lines = []
+    use_next_indent = False
 
-    def process(self):
-        self.create_trace_calls()
-        return self.construct_result()
+    for lineno, line in enumerate(lines):
+        if not line.strip():
+            new_lines.append(line)
+            continue
 
-    def create_trace_calls(self):
-        for lineno, line in enumerate(self.lines):
-            if not line.strip():  # ignore empty lines
-                continue
+        line_wo_indent = line.lstrip()
+        indent = line[:-len(line_wo_indent)]
+        first_word, remaining = extract_first_word(line_wo_indent, ' #=([{:\'"\\')
 
-            line_wo_indent = line.lstrip()
-            indent = line[:-len(line_wo_indent)]
-            first_word = rchop_by_set(line_wo_indent, ' =([{:\'"\\')  ############ possibly add # to see if first character - if so, simply return as comment
+        if use_next_indent:
+            new_lines.append(tracing_line(indent, saved_lineno, frame=True))
+            use_next_indent = False
 
-            if self.indentation_stack[-1]['indent'] == None:  # if it was'nt set/known
-                self.indentation_stack[-1]['indent'] = indent
 
-            if self.trace_calls and self.trace_calls[-1]['indent'] is None: # and  self.trace_calls[-1]['when']=='after'
-                self.trace_calls[-1]['indent'] = indent
+        if first_word in 'pass def class continue break'.split():
+            new_lines.append(tracing_line(indent, lineno, frame=True))
+            new_lines.append(line)
+        elif first_word in 'for while else'.split():
+            new_lines.append(line)
+            use_next_indent = True
+            saved_lineno = lineno
+        elif first_word in 'if elif'.split():
+            new_lines.append( indent + first_word + tracing_line(' ', lineno, frame=True) + ' and' + remaining)
+        elif '=' in line_wo_indent:
+            new_lines.append(tracing_line(indent, lineno, frame=True))
+            new_lines.append(line)
+        else:
+            new_lines.append(tracing_line(indent, lineno))
+            new_lines.append(line)
 
-            if first_word in 'for while if elif else def class try except finally with'.split():
-                if first_word in 'else elif except finally'.split():
-                    when='after-null'
-                else:
-                    when='before'
-                self.trace_calls.append(
-                    dict(
-                        when=when,
-                        place_lineno=lineno,
-                        target_lineno=lineno,
-                        indent=indent if when=='before' else None,
-                        cause=first_word,
-                    )
-                )
-                self.indentation_stack.append( dict( cause=first_word, cause_lineno=lineno, indent=None ) )
-            else:  # for ordinary statements
-               self.trace_calls.append(
-                    dict(
-                        when='before',
-                        place_lineno=lineno,
-                        target_lineno=lineno,
-                        indent=indent
-                    )
-                )
-            # check unindent
-            if self.indentation_stack[-1]['indent']:
-                while indent < self.indentation_stack[-1]['indent']:
-                    last_indent = self.indentation_stack.pop()
-                    if last_indent['cause'] in 'for while'.split():   # TODO: "for" might not be needed here (though in C++ it should)
-                        self.trace_calls.append(
-                            dict(  # could inject "before current lineno" but, less confusion -- "after previous lineno"
-                                when='after',
-                                place_lineno=lineno-1,  # might happen to be empty line -- but it's ok
-                                target_lineno=last_indent['cause_lineno'],
-                                indent=last_indent['indent']
-                            )
-                        )
 
-    def construct_result(self):
-        # for each line -- separate, what goes before, what -- after
-        restructured_trace_calls = {}
-
-        for tc in self.trace_calls:
-            key = tc['place_lineno']
-            if not key in restructured_trace_calls:
-                restructured_trace_calls[key] = {'before':[], 'after':[], 'after-null':[] }
-
-            restructured_trace_calls[key][tc['when']].append( dict(
-                    indent=tc['indent'],
-                    target_lineno=tc['target_lineno'])
-                )
-
-        result = []
-        for lineno, line in enumerate(self.lines[:-1]):  # do not include last fake instruction
-            # before
-            if lineno in restructured_trace_calls:
-                for tc in restructured_trace_calls[lineno]['before']:
-                    result.append(repr_tracepoint(tc))
-
-            # the line
-            if DEBUG_add_lineno:
-                result.append( line + "   #  %d"%lineno)
-            else:
-                result.append( line )
-            # after
-            if lineno in restructured_trace_calls:
-                for tc in restructured_trace_calls[lineno]['after']:
-                    result.append(repr_tracepoint(tc))
-                for tc in restructured_trace_calls[lineno]['after-null']:
-                    result.append(repr_tracepoint(tc))
-                    result.append(repr_static(tc))
-
-        if DEBUG:
-            return '\n'.join(result), self.lines, restructured_trace_calls, self.trace_calls
-        return '\n'.join(result)
+    return '\n'.join(new_lines)
 
 
 if __name__ == '__main__':
-    import test_sources as src
-    DEBUG = True
-    DEBUG_add_lineno = False
+    import test_highlight as src
 
-    tracer = InsertTracer(src.three_levels_if)
-    result, lines, restructured_trace_calls, trace_calls = tracer.process()
+    result = insert_highlight_info(src.test_single_move)
     print(result)
-
-    # for lineno, line in enumerate(lines[:-1]):
-    #     print(lineno, line)
-    #     print('   ', trace_calls[lineno])
-    # print(src.complex_code_result)
-    # for key in restructured_trace_calls:
-    #     for key2 in restructured_trace_calls[key]:
-    #         if restructured_trace_calls[key][key2]:
-    #             print(key, key2, restructured_trace_calls[key][key2])
-
+    print("\n", result==src.test_single_move_result)
--- src/python/test_highlight.py
+++ src/python/test_highlight.py
@@ -1,48 +1,274 @@
 import unittest
 import highlight
-import test_sources as src
+
+
+""" In the following, the functions move() and test() are considered as representative
+    functions in Reeborg's World.  When these functions are encountered, a
+    special "frame" is created.  The code execution is then displayed frame by frame.
+    Thus, one can have a correspondance between a specific frame displayed and
+    a corresponding line of code being highlighted.
+
+    Let's considered some concrete examples.   In what follows, the line number
+    for a given line of code appears at the end of the line in a comment.
+
+    The simplest example is that of a single instruction:
+
+    move()   # 1
+
+    We choose RUR.set_lineno_highlight()
+    to be the name of the function setting the line number variable.
+    Thus, after processing, this example would become
+
+    RUR.set_lineno_highlight(1)
+    move()  # 1
+
+"""
+
+test_single_move = '''
+move()
+'''
+
+test_single_move_result = '''
+RUR.set_lineno_highlight(1)
+move()
+'''
+
+""" Some instructions however will not generate a frame; yet, it might
+    still be seen as desirable to generate a frame so that the user
+    can see the code being highlighted in the editor. To do so, we use
+    an optional argument in our call to RUR.set_lineno_highlight().
+
+    We will consider only two cases:
+
+    1. single keyword instruction such as pass, continue or break
+    2. variable assignment where an equality sign is used.
+"""
+
+test_pass = '''
+pass
+'''
+
+test_pass_result = '''
+RUR.set_lineno_highlight(1, frame=True)
+pass
+'''
+
+test_single_assignment_1 = '''
+x=2
+'''
+
+test_single_assignment_1_result = '''
+RUR.set_lineno_highlight(1, frame=True)
+x=2
+'''
+
+test_single_assignment_2 = '''
+   x  =  2
+'''
+
+test_single_assignment_2_result = '''
+   RUR.set_lineno_highlight(1, frame=True)
+   x  =  2
+'''
+
+"""
+    Next, we consider function definitions:
+"""
+
+test_def = '''
+def twice():
+    move()
+    move()
+'''
+
+test_def_result = '''
+RUR.set_lineno_highlight(1, frame=True)
+def twice():
+    RUR.set_lineno_highlight(2)
+    move()
+    RUR.set_lineno_highlight(3)
+    move()
+'''
+
+""" Classes should follow the same pattern """
+
+test_class = '''
+class Robot(UsedRobot):
+
+    def turn_right(self):
+        turn_left()
+        turn_left()
+        turn_left()
+
+    def to_do():
+        pass
+'''
+
+test_class_result = '''
+RUR.set_lineno_highlight(1, frame=True)
+class Robot(UsedRobot):
+
+    RUR.set_lineno_highlight(3, frame=True)
+    def turn_right(self):
+        RUR.set_lineno_highlight(4)
+        turn_left()
+        RUR.set_lineno_highlight(5)
+        turn_left()
+        RUR.set_lineno_highlight(6)
+        turn_left()
+
+    RUR.set_lineno_highlight(8, frame=True)
+    def to_do():
+        RUR.set_lineno_highlight(9, frame=True)
+        pass
+'''
+
+"""  Basic for and while loops present a certain challenge in that
+    the line numbering information must indicate that they possibly
+    go back to the beginning of the loop multiple times.
+    This is accomplished with having a frame pointing to the for loop
+    at the top of the loop.
+
+"""
+
+test_for = '''
+for i in range(n):
+    move()
+    move()
+move()
+'''
+
+test_for_result = '''
+for i in range(n):
+    RUR.set_lineno_highlight(1, frame=True)
+    RUR.set_lineno_highlight(2)
+    move()
+    RUR.set_lineno_highlight(3)
+    move()
+RUR.set_lineno_highlight(4)
+move()
+'''
+
+test_while = '''
+while test():
+    move()
+    move()
+move()
+'''
+
+test_while_result = '''
+while test():
+    RUR.set_lineno_highlight(1, frame=True)
+    RUR.set_lineno_highlight(2)
+    move()
+    RUR.set_lineno_highlight(3)
+    move()
+RUR.set_lineno_highlight(4)
+move()
+'''
+
+"""
+    Simple if statements are easy to consider:
+
+    if test():   # 11
+        move()   # 12
+
+    might be thought as being transformed as follows:
+
+    RUR.set_lineno_highlight(11)
+    if test():  # 11
+        RUR.set_lineno_highlight(12)
+        move()   # 12
+
+    However, for line 11 to be highlighted, we need to make sure that
+    the test() function results in a frame being recorded.
+    If this is not the case, then line 11 will appear as being skipped over.
+    This could be handled by using RUR.set_lineno_highlight() in the
+    following way:
+
+    if RUR.set_lineno_highlight(11, frame=True) and test():  # 11
+        RUR.set_lineno_highlight(12)
+        move()   # 12
+
+    The (small) disadvantage of this approach is that the evaluation of the
+    if statement could require two frames instead of one
+    which might be confusing if the execution of the program is
+    done by pressing the "step" button; however, we think it is
+    something worthwhile.
+
+"""
+
+test_if = '''
+if test():
+    move()
+'''
+
+test_if_result = '''
+if RUR.set_lineno_highlight(1, frame=True) and test():
+    RUR.set_lineno_highlight(2)
+    move()
+'''
+
+test_if_else = '''
+if test():
+    move()
+else:
+    pass
+'''
+
+test_if_else_result = '''
+if RUR.set_lineno_highlight(1, frame=True) and test():
+    RUR.set_lineno_highlight(2)
+    move()
+else:
+    RUR.set_lineno_highlight(3, frame=True)
+    RUR.set_lineno_highlight(4, frame=True)
+    pass
+'''
+
 
 class TestSequenceFunctions(unittest.TestCase):
 
-    def test_simple(self):
-        tracer = highlight.InsertTracer(src.simple)
-        result = tracer.process()
-        self.assertEqual(src.simple_result, result)
+    def test_single_move(self):
+        result = highlight.insert_highlight_info(test_single_move)
+        self.assertEqual(test_single_move_result, result)
 
-    def test_simple_no_spaces(self):
-        tracer = highlight.InsertTracer(src.simple_1)
-        result = tracer.process()
-        self.assertEqual(src.simple_result_1, result)
+    def test_pass(self):
+        result = highlight.insert_highlight_info(test_pass)
+        self.assertEqual(test_pass_result, result)
+
+    def test_single_assignment(self):
+        result = highlight.insert_highlight_info(test_single_assignment_1)
+        self.assertEqual(test_single_assignment_1_result, result)
+
+        result = highlight.insert_highlight_info(test_single_assignment_2)
+        self.assertEqual(test_single_assignment_2_result, result)
 
     def test_def(self):
-        tracer = highlight.InsertTracer(src.def_code)
-        result = tracer.process()
-        self.assertEqual(src.def_code_result, result)
+        result = highlight.insert_highlight_info(test_def)
+        self.assertEqual(test_def_result, result)
+
+    def test_class(self):
+        result = highlight.insert_highlight_info(test_class)
+        self.assertEqual(test_class_result, result)
+
+    def test_for(self):
+        result = highlight.insert_highlight_info(test_for)
+        self.assertEqual(test_for_result, result)
 
     def test_while(self):
-        tracer = highlight.InsertTracer(src.while_loop)
-        result = tracer.process()
-        self.assertEqual(src.while_loop_result, result)
-
-    def test_only_while(self):
-        tracer = highlight.InsertTracer(src.only_while_loop)
-        result = tracer.process()
-        self.assertEqual(src.only_while_loop_result, result)
+        result = highlight.insert_highlight_info(test_while)
+        self.assertEqual(test_while_result, result)
 
     def test_if(self):
-        tracer = highlight.InsertTracer(src.if_elif_else)
-        result = tracer.process()
-        self.assertEqual(src.if_elif_else_result, result)
+        result = highlight.insert_highlight_info(test_if)
+        self.assertEqual(test_if_result, result)
 
-    def test_complex(self):
-        tracer = highlight.InsertTracer(src.complex_code)
-        result = tracer.process()
-        self.assertEqual(src.complex_code_result, result)
+    def test_if_else(self):
+        result = highlight.insert_highlight_info(test_if_else)
+        self.assertEqual(test_if_else_result, result)
 
-    def test_three_levels_if(self):
-        tracer = highlight.InsertTracer(src.three_levels_if)
-        result = tracer.process()
-        self.assertEqual(src.three_levels_if_result, result)
+
 
 if __name__ == '__main__':
     unittest.main()
