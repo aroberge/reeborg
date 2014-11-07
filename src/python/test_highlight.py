e9fc3fcdd8bd1a07c1c315c6b73b2ae7ff235341
andre.roberge@gmail.com <andre.roberge@gmail.com@ec9b96ea-facd-7dc9-34b9-829c92dc2a88>
2014-11-07 00:58:27 +0000

    Basic module to provide highlighting hooks with tests appears to be complete, covering all essential Python keywords and constructs - but limited to a single line at a time.

--- src/python/test_highlight.py
+++ src/python/test_highlight.py
@@ -129,6 +129,11 @@ class Robot(UsedRobot):
     This is accomplished with having a frame pointing to the for loop
     at the top of the loop.
 
+    To make sure that there is some indication that the loop is
+    executed, we add a frame before as well ... this means that
+    it will take frames to enter a loop, highlighting the for or while
+    statement twice in a row the first time through.
+
 """
 
 test_for = '''
@@ -139,6 +144,7 @@ move()
 '''
 
 test_for_result = '''
+RUR.set_lineno_highlight(1, frame=True)
 for i in range(n):
     RUR.set_lineno_highlight(1, frame=True)
     RUR.set_lineno_highlight(2)
@@ -157,6 +163,7 @@ move()
 '''
 
 test_while_result = '''
+RUR.set_lineno_highlight(1, frame=True)
 while test():
     RUR.set_lineno_highlight(1, frame=True)
     RUR.set_lineno_highlight(2)
@@ -173,20 +180,10 @@ move()
     if test():   # 11
         move()   # 12
 
-    might be thought as being transformed as follows:
+    can be simply as being transformed as follows:
 
-    RUR.set_lineno_highlight(11)
+    RUR.set_lineno_highlight(11, frame=True)
     if test():  # 11
-        RUR.set_lineno_highlight(12)
-        move()   # 12
-
-    However, for line 11 to be highlighted, we need to make sure that
-    the test() function results in a frame being recorded.
-    If this is not the case, then line 11 will appear as being skipped over.
-    This could be handled by using RUR.set_lineno_highlight() in the
-    following way:
-
-    if RUR.set_lineno_highlight(11, frame=True) and test():  # 11
         RUR.set_lineno_highlight(12)
         move()   # 12
 
@@ -204,7 +201,8 @@ if test():
 '''
 
 test_if_result = '''
-if RUR.set_lineno_highlight(1, frame=True) and test():
+RUR.set_lineno_highlight(1, frame=True)
+if test():
     RUR.set_lineno_highlight(2)
     move()
 '''
@@ -217,7 +215,8 @@ else:
 '''
 
 test_if_else_result = '''
-if RUR.set_lineno_highlight(1, frame=True) and test():
+RUR.set_lineno_highlight(1, frame=True)
+if test():
     RUR.set_lineno_highlight(2)
     move()
 else:
@@ -225,6 +224,253 @@ else:
     RUR.set_lineno_highlight(4, frame=True)
     pass
 '''
+
+""" elif are a bit tricker to handle.  One the one hand, we can not insert
+    a statement just before them and expect it to be executed.  On the other hand,
+    if we insert something after, and the condition is determined to be False,
+    so that we'd go to the next elif or else branch, we'd never go inside the body
+    and have the highlighting statement be executed.
+    A way out of this is to insert the highlighting statement as an additional
+    logical test to be performed, but guaranteed to be always True as
+    follows.
+"""
+
+test_if_elif_else = '''
+if test():
+    move()
+elif other_test():
+    turn_left()
+else:
+    pass
+'''
+
+test_if_elif_else_result = '''
+RUR.set_lineno_highlight(1, frame=True)
+if test():
+    RUR.set_lineno_highlight(2)
+    move()
+elif RUR.set_lineno_highlight(3, frame=True) and other_test():
+    RUR.set_lineno_highlight(4)
+    turn_left()
+else:
+    RUR.set_lineno_highlight(5, frame=True)
+    RUR.set_lineno_highlight(6, frame=True)
+    pass
+'''
+
+""" So far, these are the Python keywords that could appear as
+    the first "word" in a statement and that we have covered:
+
+    def, for, if, elif, else, while, class, pass
+
+    The remaining keywords which can appear at the beginning of a statement are
+
+    break, continue, del, except, finally, from, global, import, nonlocal,
+    raise, return, try, with, yield
+
+    Those that could not appear at the beginning of a statement are:
+
+    False, None, True, and, as, in, lambda, not, or
+
+    Let's consider examples from the Python tutorial for the remaining
+    keywords that we have to cover.
+"""
+
+# Note : the following also tests comments
+
+test_break = '''
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+    else:
+        # loop fell through without finding a factor
+        print(n, 'is a prime number')
+'''
+
+test_break_result = '''
+RUR.set_lineno_highlight(1, frame=True)
+for n in range(2, 10):
+    RUR.set_lineno_highlight(1, frame=True)
+    RUR.set_lineno_highlight(2, frame=True)
+    for x in range(2, n):
+        RUR.set_lineno_highlight(2, frame=True)
+        RUR.set_lineno_highlight(3, frame=True)
+        if n % x == 0:
+            RUR.set_lineno_highlight(4)
+            print(n, 'equals', x, '*', n//x)
+            RUR.set_lineno_highlight(5, frame=True)
+            break
+    else:
+        RUR.set_lineno_highlight(6, frame=True)
+        # loop fell through without finding a factor
+        RUR.set_lineno_highlight(8)
+        print(n, 'is a prime number')
+'''
+
+test_continue = '''
+for num in range(2, 10):
+    if num % 2 == 0:
+        print("Found an even number", num)
+        continue
+    print("Found a number", num)
+'''
+
+test_continue_result = '''
+RUR.set_lineno_highlight(1, frame=True)
+for num in range(2, 10):
+    RUR.set_lineno_highlight(1, frame=True)
+    RUR.set_lineno_highlight(2, frame=True)
+    if num % 2 == 0:
+        RUR.set_lineno_highlight(3)
+        print("Found an even number", num)
+        RUR.set_lineno_highlight(4, frame=True)
+        continue
+    RUR.set_lineno_highlight(5)
+    print("Found a number", num)
+'''
+
+test_from_import = '''
+import this
+from this import that
+'''
+
+test_from_import_result = '''
+RUR.set_lineno_highlight(1, frame=True)
+import this
+RUR.set_lineno_highlight(2, frame=True)
+from this import that
+'''
+
+test_try_except_finally = '''
+def divide(x, y):
+    try:
+        result = x / y
+    except ZeroDivisionError:
+        print("division by zero!")
+    else:
+        print("result is", result)
+    finally:
+        print("executing finally clause")
+'''
+
+
+test_try_except_finally_result = '''
+RUR.set_lineno_highlight(1, frame=True)
+def divide(x, y):
+    RUR.set_lineno_highlight(2, frame=True)
+    try:
+        RUR.set_lineno_highlight(3, frame=True)
+        result = x / y
+    except ZeroDivisionError:
+        RUR.set_lineno_highlight(4, frame=True)
+        RUR.set_lineno_highlight(5)
+        print("division by zero!")
+    else:
+        RUR.set_lineno_highlight(6, frame=True)
+        RUR.set_lineno_highlight(7)
+        print("result is", result)
+    finally:
+        RUR.set_lineno_highlight(8, frame=True)
+        RUR.set_lineno_highlight(9)
+        print("executing finally clause")
+'''
+
+test_return = '''
+def twice(x):
+    return x*2
+'''
+
+test_return_result = '''
+RUR.set_lineno_highlight(1, frame=True)
+def twice(x):
+    RUR.set_lineno_highlight(2, frame=True)
+    return x*2
+'''
+
+test_del = '''
+del x
+'''
+
+test_del_result = '''
+RUR.set_lineno_highlight(1, frame=True)
+del x
+'''
+
+test_global_nonlocal = '''
+def twice(x):
+    global x
+    nonlocal y
+    return x*2
+'''
+
+test_global_nonlocal_result = '''
+RUR.set_lineno_highlight(1, frame=True)
+def twice(x):
+    RUR.set_lineno_highlight(2)
+    global x
+    RUR.set_lineno_highlight(3)
+    nonlocal y
+    RUR.set_lineno_highlight(4, frame=True)
+    return x*2
+'''
+
+test_raise = '''
+try:
+    raise NameError('HiThere')
+except NameError:
+    print('An exception flew by!')
+    raise
+'''
+
+test_raise_result = '''
+RUR.set_lineno_highlight(1, frame=True)
+try:
+    RUR.set_lineno_highlight(2, frame=True)
+    raise NameError('HiThere')
+except NameError:
+    RUR.set_lineno_highlight(3, frame=True)
+    RUR.set_lineno_highlight(4)
+    print('An exception flew by!')
+    RUR.set_lineno_highlight(5, frame=True)
+    raise
+'''
+
+test_with = '''
+with A() as a, B() as b:
+    suite
+'''
+
+test_with_result = '''
+RUR.set_lineno_highlight(1, frame=True)
+with A() as a, B() as b:
+    RUR.set_lineno_highlight(2)
+    suite
+'''
+
+test_yield = '''
+def get_primes(number):
+    while True:
+        if is_prime(number):
+            yield number
+        number += 1
+'''
+
+test_yield_result = '''
+RUR.set_lineno_highlight(1, frame=True)
+def get_primes(number):
+    RUR.set_lineno_highlight(2, frame=True)
+    while True:
+        RUR.set_lineno_highlight(2, frame=True)
+        RUR.set_lineno_highlight(3, frame=True)
+        if is_prime(number):
+            RUR.set_lineno_highlight(4, frame=True)
+            yield number
+        RUR.set_lineno_highlight(5, frame=True)
+        number += 1
+'''
+
 
 
 class TestSequenceFunctions(unittest.TestCase):
@@ -269,6 +515,50 @@ class TestSequenceFunctions(unittest.TestCase):
         self.assertEqual(test_if_else_result, result)
 
 
+    def test_if_elif_else(self):
+        result = highlight.insert_highlight_info(test_if_elif_else)
+        self.assertEqual(test_if_elif_else_result, result)
+
+    def test_break(self):
+        # test comments as well
+        result = highlight.insert_highlight_info(test_break)
+        self.assertEqual(test_break_result, result)
+
+    def test_continue(self):
+        result = highlight.insert_highlight_info(test_continue)
+        self.assertEqual(test_continue_result, result)
+
+    def test_from_import(self):
+        result = highlight.insert_highlight_info(test_from_import)
+        self.assertEqual(test_from_import_result, result)
+
+    def test_try_except_finally(self):
+        result = highlight.insert_highlight_info(test_try_except_finally)
+        self.assertEqual(test_try_except_finally_result, result)
+
+    def test_return(self):
+        result = highlight.insert_highlight_info(test_return)
+        self.assertEqual(test_return_result, result)
+
+    def test_del(self):
+        result = highlight.insert_highlight_info(test_del)
+        self.assertEqual(test_del_result, result)
+
+    def test_global_nonlocal(self):
+        result = highlight.insert_highlight_info(test_global_nonlocal)
+        self.assertEqual(test_global_nonlocal_result, result)
+
+    def test_raise(self):
+        result = highlight.insert_highlight_info(test_raise)
+        self.assertEqual(test_raise_result, result)
+
+    def test_with(self):
+        result = highlight.insert_highlight_info(test_with)
+        self.assertEqual(test_with_result, result)
+
+    def test_yield(self):
+        result = highlight.insert_highlight_info(test_yield)
+        self.assertEqual(test_yield_result, result)
 
 if __name__ == '__main__':
     unittest.main()
