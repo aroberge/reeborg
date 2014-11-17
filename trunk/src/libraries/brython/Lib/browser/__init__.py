d63af7b4201ebd482e7c85ba47273f8181a67d20
andre.roberge@gmail.com <andre.roberge@gmail.com@ec9b96ea-facd-7dc9-34b9-829c92dc2a88>
2014-11-17 03:47:12 +0000

    removing old versions

--- trunk/src/libraries/brython/Lib/browser/__init__.py
+++
@@ -1,1 +0,0 @@
-from _browser import *
--- trunk/src/libraries/brython/libs/_posixsubprocess.js
+++
@@ -1,7 +0,0 @@
-var $module = (function($B){
-
-    var __builtins__ = $B.builtins
-    return {
-       cloexec_pipe: function() {}   // fixme
-    }
-})(__BRYTHON__)
