dd7ddd68eb1bf615abc083bdf2443de2c36ccb4a
André Roberge <andre.roberge@gmail.com>
2016-03-12 07:18:53 -0400

    minor updating from local version prior to merging pull request

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,12 @@ require("./commands.js");
 require("./world_editor.js");
 
 require("./start_session.js");
+
+// TODO: easy function to hide edit world button (to prevent students seeing hidden code)
+// TODO: animated robots/ decorative objects, objects
+// TODO: document all world-editing functions, make them directly available as methods of RUR.
+//       Use jsdoc and put on site.
+// TODO: add turtle mode (see blockly for comparing with expected solution); ensure a blockly counterpart
+// TODO: implement paint() and colour_here() in Blockly
+// TODO: Change world "Alone" so that it sets/restores default robot images
+// TODO: Create offline version as part of the build sequence
