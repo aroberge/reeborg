50703b52a160b3bd898d07f1c58a4efc6057e4ee
MuhunKim <yale2a11@gmail.com>
2016-08-10 21:40:57 +0900

    typo, add translation'

--- src/lang/ui_ko.js
+++ src/lang/ui_ko.js
@@ -1,12 +1,12 @@
 RUR.ui_ko = {};
 RUR.ko_to_en = {};
 
-RUR.ui_ko["ko-en"] = "혼용된 모드: 사용자 환경은 한국어로 번역되어 있지만, 프로그래밍 언어는 영어로 이루어져 있습니다.<br>" +
-    "Mixed mode: User Interface in Korean; programming language in English.<br>";
+RUR.ui_ko["ko-en"] = "혼용된 모드: 사용자 환경은 한국어로 번역되어 있지만, 프로그래밍 언어는 영어로 이루어져 있습니다. <br>" +
+ "Mixed mode: User Interface in Korean; programming language in English.<br>";
 
-RUR.ui_ko["SITE NAME"] = "리보그의 월드";
+RUR.ui_ko["SITE NAME"] = "리보그의 세상";
 RUR.ui_ko["WORLD INFO"] = "월드 정보";
-RUR.ui_ko["EDITOR VISIBLE"] = "에디터 유지시키기";
+RUR.ui_ko["EDITOR VISIBLE"] = "에디터 유지하기";
 
 
 RUR.ui_ko["apple"] = "사과";
@@ -19,8 +19,8 @@ RUR.ui_ko["bridge"] = "다리";
 RUR.ko_to_en["다리"] = "bridge";
 RUR.ui_ko["carrot"] = "당근";
 RUR.ko_to_en["당근"] = "carrot";
-RUR.ui_ko["daisy"] = "데이지꽃";
-RUR.ko_to_en["데이지꽃"] = "daisy";
+RUR.ui_ko["daisy"] = "데 이지 꽃";
+RUR.ko_to_en["데 이지 꽃"] = "daisy";
 RUR.ui_ko["dandelion"] = "민들레";
 RUR.ko_to_en["민들레"] = "dandelion";
 RUR.ui_ko["leaf"] = "잎";
@@ -40,31 +40,31 @@ RUR.ko_to_en["삼각형"] = "triangle";
 RUR.ui_ko["tulip"] = "튤립";
 RUR.ko_to_en["튤립"] = "tulip";
 
-RUR.ui_ko["Problem with onload code."] = "유효하지 않는 자바스크립트 onload 코드입니다; 이 월드의 제작자에게 연락하세요.";
+RUR.ui_ko["Problem with onload code."] = "유효하지 않은 자바스크립트 onload 코드입니다; 이 월드의 제작자에게 연락하세요.";
 
 RUR.ui_ko["Too many steps:"] = "너무 많은 steps: {max_steps}";
-RUR.ui_ko["<li class='success'>Reeborg is at the correct x position.</li>"] = "<li class='success'>리보그는 올바른 x 위치에 있습니다.</li>";
-RUR.ui_ko["<li class='failure'>Reeborg is at the wrong x position.</li>"] = "<li class='failure'>리보그는 잘못된 x 위치에 있습니다.</li>";
-RUR.ui_ko["<li class='success'>Reeborg is at the correct y position.</li>"] = "<li class='success'>리보그는 올바른 y 위치에 있습니다.</li>";
-RUR.ui_ko["<li class='failure'>Reeborg is at the wrong y position.</li>"] = "<li class='failure'>리보그는 잘못된 y 위치에 있습니다.</li>";
-RUR.ui_ko["<li class='success'>All objects are at the correct location.</li>"] = "<li class='success'>모든 객체가 올바른 위치에 있습니다.</li>";
+RUR.ui_ko["<li class='success'>Reeborg is at the correct x position.</li>"] = "<li class='success'>리보그는 올바른 x 위치에 있습니다. </li>";
+RUR.ui_ko["<li class='failure'>Reeborg is at the wrong x position.</li>"] = "<li class='failure'>리보그는 잘못된 x 위치에 있습니다. </li>";
+RUR.ui_ko["<li class='success'>Reeborg is at the correct y position.</li>"] = "<li class='success'>리보그는 올바른 y 위치에 있습니다. </li>";
+RUR.ui_ko["<li class='failure'>Reeborg is at the wrong y position.</li>"] = "<li class='failure'>리보그는 잘못된 y 위치에 있습니다. </li>";
+RUR.ui_ko["<li class='success'>All objects are at the correct location.</li>"] = "<li class='success'>모든 객체가 올바른 위치에 있습니다. </li>";
 RUR.ui_ko["<li class='failure'>One or more objects are not at the correct location.</li>"] = "<li class='failure'>하나 이상의 객체가 올바른 위치에 있지 않습니다.</li>";
-RUR.ui_ko["<li class='success'>All walls have been built correctly.</li>"] = "<li class='success'>모든 벽은 제대로 지어지고 있습니다.</li>";
-RUR.ui_ko["<li class='failure'>One or more walls missing or built at wrong location.</li>"] = "<li class='failure'>하나 이상의 벽이 누락되거나 잘못된 위치에서 건설됬습니다.</li>";
-RUR.ui_ko["Last instruction completed!"] = "마지막 명령이 완료됬습니다!";
+RUR.ui_ko["<li class='success'>All walls have been built correctly.</li>"] = "<li class='success'>모든 벽은 제대로 지어지고 있습니다. </li>";
+RUR.ui_ko["<li class='failure'>One or more walls missing or built at wrong location.</li>"] = "<li class='failure'>하나 이상의 벽이 빠지거나 잘못된 위치에서 건설됐습니다/li>";
+RUR.ui_ko["Last instruction completed!"] = "마지막 명령이 완료됐습니다!";
 RUR.ui_ko["<p class='center'>Instruction <code>done()</code> executed.</p>"] = "<p class='center'>명령 <code>done()</code> 실행.</p>";
 
 RUR.ui_ko["Unknown object"] = "알 수 없는 객체: {obj}";
-RUR.ui_ko["No object found here"] = "여기서 {obj} 를 찾을수 없어요!";
+RUR.ui_ko["No object found here"] = "여기서 {obj} 를 찾을 수 없어요!";
 RUR.ui_ko["object"] = "객체";
 RUR.ui_ko["I don't have any object to put down!"] = "나는 집어넣을 {obj} 가 없어요!";
 RUR.ui_ko["There is already a wall here!"] = "벽이 여기에 이미 있어요!";
-RUR.ui_ko["Ouch! I hit a wall!"] = "아으,, 아파요! 저는 벽을 부딛쳤어요!";
+RUR.ui_ko["Ouch! I hit a wall!"] = "아으, 아파요! 저는 벽을 부딪혔어요!";
 RUR.ui_ko["Done!"] = "끝!";
 RUR.ui_ko["There is no position as a goal in this world!"] = "위치에 대한 목표가 없어요!";
 RUR.ui_ko["There is no goal in this world!"] = "이 월드는 목표가 없어요.";
 RUR.ui_ko["I carry too many different objects. I don't know which one to put down!"] = "저는 너무 많은 다른 객체들을 싣고 있어요. 저는 이중 어떤 걸 내려놓을지 모르겠어요!";
-RUR.ui_ko["Many objects are here; I do not know which one to take!"] = "많은 객체들이 여기에 있어요; 저는 그 중 어떤걸 가져갈지 모르겠어요!";
+RUR.ui_ko["Many objects are here; I do not know which one to take!"] = "많은 객체가 여기에 있어요; 저는 그 중 어떤 걸 가져갈지 모르겠어요!";
 
 RUR.ui_ko.east = "동쪽";
 RUR.ui_ko.north = "북쪽";
@@ -73,10 +73,10 @@ RUR.ui_ko.south = "남쪽";
 RUR.ui_ko["Unknown orientation for robot."] = "로봇의 방향을 알 수 없습니다.";
 
 RUR.ui_ko["World selected"] = "월드 {world} 가 선택되었습니다";
-RUR.ui_ko["Could not find world"] = "월드를 찾을 수 없습니다 {world}";
+RUR.ui_ko["Could not find world"] = "월드를 찾을 수 없습니다. {world}";
 RUR.ui_ko["Object names"] = " 라이브러리, 토큰, 별, 삼각형, 사각형, 등.";
 
-RUR.ui_ko["Invalid world file."] = "유효하지 않는 월드 파일.";
+RUR.ui_ko["Invalid world file."] = "유효하지 않은 월드 파일.";
 RUR.ui_ko["Could not find link: "] = "링크를 찾을 수 없습니다: ";
 
 RUR.ui_ko["Click on world to move robot."] = "월드를 클릭해서 추가하거나 시작 가능한 리보그 위치를 제거합니다.";
@@ -87,18 +87,18 @@ RUR.ui_ko["Click on world to add object."] = "{obj} 의 수를 설정하기 위�
 RUR.ui_ko["Click on desired object below."] = "아래에서 원하는 개체를 클릭합니다.";
 RUR.ui_ko["Click on world to toggle walls."] = "벽을 달기 위해 월드를 클락하세요.";
 RUR.ui_ko["Click on world to set home position for robot."] = "로봇의 최종위치를 정하기 위해 월드를 클릭하세요.";
-RUR.ui_ko["Click on world to toggle additional walls to build."] = "추가적으로 벽을 달기 위해 월드를 클릭하세요.";
+RUR.ui_ko["Click on world to toggle additional walls to build."] = "추가로 벽을 달기 위해 월드를 클릭하세요.";
 RUR.ui_ko["Click on desired goal object below."] = "아래에서 원하는 목표 객체를 클릭하세요.";
 RUR.ui_ko["Click on world to set number of goal objects."] = "{obj}의 목표를 설정하기 위해 객체를 클릭하세요.";
 RUR.ui_ko["Enter number of tokens for robot to carry (use inf for infinite number)"] = "싣고 갈 토큰의 수를 입력하세요.";
-RUR.ui_ko[" is not a valid value!"] = " 유효하지 않는 값입니다!";
+RUR.ui_ko[" is not a valid value!"] = " 유효하지 않은 값입니다!";
 RUR.ui_ko["Enter number of objects desired at that location."] = "{obj} 의 수를 설정하기 위해 월드를 클릭하세요.";
 RUR.ui_ko["Objects found here:"] = "객체를 여기서 찾음:";
 RUR.ui_ko["Description"] = "설명";
-RUR.ui_ko["A robot located here carries no objects."] = "로봇은 {x},{y} 에 위치해 있고 싣고 있는 객체는 없습니다.";
+RUR.ui_ko["A robot located here carries no objects."] = "로봇은 {x},{y}에 위치해 있고 싣고 있는 객체는 없습니다.";
 RUR.ui_ko["Goal to achieve:"] = "목표 달성:";
-RUR.ui_ko["A robot located here carries:"] = "로봇은 {x},{y} 에 위치해 있습니다. 싣고 있는 객체:";
-RUR.ui_ko["random location"] = "랜덤 위치";
+RUR.ui_ko["A robot located here carries:"] = "로봇은 {x},{y}에 위치해 있습니다. 싣고 있는 객체:";
+RUR.ui_ko["random location"] = "무작위 위치";
 RUR.ui_ko["Enter number of objects to give to robot."] = "로봇에게 주기 위해 {obj} 의 수를 입력하세요..";
 RUR.ui_ko["Special information about this location:"] = "이 위치에 대한 특별한 정보:";
 RUR.ui_ko["Click on world to toggle tile."] = "{obj} 타일을 달기 위해 월드를 클릭하세요.";
@@ -108,10 +108,10 @@ RUR.ui_ko["water"] = "물";
 RUR.ui_ko["grass"] = "잔디";
 RUR.ui_ko["gravel"] = "자갈";
 RUR.ui_ko["ice"] = "얼음";
-RUR.ui_ko["A wall must be built east of this location."] = "벽은 이 위치의 동쪽에 지어져야 됩니다.";
-RUR.ui_ko["A wall must be built north of this location."] = "벽은 이 위치의 북쪽에 지어져야 됩니다.";
-RUR.ui_ko["A wall must be built west of this location."] = "벽은 이 위치의 서쪽에 지어져야 됩니다.";
-RUR.ui_ko["A wall must be built south of this location."] = "벽은 이 위치의 남쪽에 지어져야 됩니다.";
+RUR.ui_ko["A wall must be built east of this location."] = "벽은 이 위치의 동쪽에 지어져야 합니다.";
+RUR.ui_ko["A wall must be built north of this location."] = "벽은 이 위치의 북쪽에 지어져야 합니다.";
+RUR.ui_ko["A wall must be built west of this location."] = "벽은 이 위치의 서쪽에 지어져야 합니다.";
+RUR.ui_ko["A wall must be built south of this location."] = "벽은 이 위치의 남쪽에 지어져야 합니다.";
 RUR.ui_ko["The final required position of the robot will be chosen at random."] = "로봇의 마지막으로 필요한 위치가 무작위로 선택됩니다.";
 RUR.ui_ko["The final position of the robot must be (x, y) = "] = "로봇의 최종위치는 반드시 (x, y) = ";
 RUR.ui_ko["Click on world to fill with given tile."] = "주어진 타일을 채우기 위해 월드를 클릭합니다.";
@@ -123,7 +123,7 @@ RUR.ui_ko["colour"] = "색";
 
 RUR.ui_ko["Name already exist; confirm that you want to replace its content."] = "이름이 이미 존재합니다; 당신이 내용을 교체하고 싶으면 확인합니다.";
 RUR.ui_ko["No such world!"] = "월드가 존재하지 않습니다!";
-RUR.ui_ko["Enter world name to save"] = "월드를 저장하기 위해 월드 이름을 입력 해주세요; 사용될 이름: ";
+RUR.ui_ko["Enter world name to save"] = "월드를 저장하기 위해 월드 이름을 입력해주세요; 사용될 이름: ";
 RUR.ui_ko["Enter world name to delete"] = "월드를 삭제하기 위해 월드 이름을 입력 해주세요; 기존 세계: ";
 RUR.ui_ko["Delete "] = "삭제 ";
 
@@ -182,8 +182,8 @@ RUR.ui_ko["Delay between actions; default is 300 ms."] = "행동을 지연시킵
 RUR.ui_ko["Save world in browser"] = "월드를 브라우저에 저장하기";
 RUR.ui_ko["Save permalink"] = "퍼머 저장";
 RUR.ui_ko["Save permalink explanation"] = "파일의 퍼머링크 복사본을 저장하기";
-RUR.ui_ko["LOAD BLOCKLY"] = "Import program (blocks) from file";
-RUR.ui_ko["LOAD BLOCKLY EXPLAIN"] = "Opens a local file and use its content to replace the content of the Blockly workspace.";
+RUR.ui_ko["LOAD BLOCKLY"] = "프로그램(블럭들)을 파일에서 불러오기";
+RUR.ui_ko["LOAD BLOCKLY EXPLAIN"] = "로컬 파일을 열고 Blockly 작업공간의 요소 대신에 이 요소를 사용합니다";
 RUR.ui_ko["LOAD EDITOR"] = "파일로 불러오기";
 RUR.ui_ko["LOAD EDITOR EXPLAIN"] = "로컬 저장소에서 소스코드 불러오기";
 RUR.ui_ko["LOAD LIBRARY"] = "파일에서 라이브러리를 가져오기";
@@ -249,7 +249,7 @@ RUR.ui_ko["WORLD CREATION TITLE"] = "월드 : 창조, 수정..";
 RUR.ui_ko["EDIT WORLD"] = "월드 수정";
 RUR.ui_ko["EDIT WORLD EXPLAIN"] = "기존 월드를 수정하여 자신 만의 월드를 만들 수 있습니다.";
 RUR.ui_ko["PROGRAM IN EDITOR"] = "에디터";
-RUR.ui_ko["PROGRAM IN BLOCKLY WORKSPACE"] = "Program in blockly workspace";
+RUR.ui_ko["PROGRAM IN BLOCKLY WORKSPACE"] = "blockly 작업 공간 프로그램";
 RUR.ui_ko["SPECIAL EXECUTION"] = "미래에 생겨날 기능";
 RUR.ui_ko["REVERSE STEP EXPLAIN"] = "이전 실행 상태를 되돌립니다.";
 RUR.ui_ko["ERASE TRACE"] = "흔적 지우기";
@@ -262,7 +262,7 @@ RUR.ui_ko["DOCUMENTATION"] = '<a href="http://reeborg.ca/docs/ko" target="_blank
 RUR.ui_ko["PYTHON HELP"] = "파이썬을 사용해서, <code>help()</code>를 실행해서 명령어의 목록을 얻으세요 또는 <code>help(함수명)</code>으로 해당 <code>함수명()</code>의 정보를 확인할 수 있습니다. 예를 들어, <code>help(move)</code>로 <code>move</code>함수의 정보를 얻을 수 있습니다.";
 RUR.ui_ko["KEYBOARD HELP"] = "리보그의 키보드를 클릭해서 파이썬 키워드 등, 사용할수 있는 명령어의 목록을 보세요.";
 
-RUR.ui_ko["WORLD EDITOR"] = "월드 에디터";
+RUR.ui_ko["WORLD EDITOR"] = "월드 편집기";
 RUR.ui_ko["m-east"] = "동쪽";
 RUR.ui_ko["m-north"] = "북쪽";
 RUR.ui_ko["m-west"] = "서쪽";
