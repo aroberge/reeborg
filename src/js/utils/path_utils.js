bdf93613dd8c3d50dca6721614cac449ad3ebdca
André Roberge <andre.roberge@gmail.com>
2017-11-19 18:05:39 -0400

    implemented #388

--- src/js/utils/path_utils.js
+++ src/js/utils/path_utils.js
@@ -16,15 +16,24 @@ require("./../drawing/visible_world.js");
 
 RUR.print_path = function () {
     "use strict";
-    var history, i, path, world, prev_x, prev_y, x, y;
+    var history, path, world, x_init, y_init;
 
     world = RUR.get_current_world();
     if (world.robots === undefined || world.robots.length === 0) {
         throw new RUR.ReeborgError("Missing robot; cannot print path.");
     }
     history = world.robots[0]._trace_history;
-    prev_x = world.robots[0].initial_position[0];
-    prev_y = world.robots[0].initial_position[1];
+    x_init = world.robots[0].initial_position[0];
+    y_init = world.robots[0].initial_position[1];
+    path = compute_path(x_init, y_init, history);
+    RUR._write_ln(path);
+};
+
+function compute_path(x_init, y_init, history) {
+    var x, y, prev_x, prev_y, path;
+
+    prev_x = x_init;
+    prev_y = y_init;
     path = [[prev_x, prev_y]];
 
     for (i=0; i < history.length; i++) {
@@ -36,8 +45,8 @@ RUR.print_path = function () {
             prev_y = y;
         }
     }
-    RUR._write_ln(path);
-};
+    return path;
+}
 
 /** @function check_path
  * @memberof RUR
@@ -45,54 +54,77 @@ RUR.print_path = function () {
  * @summary This function compares the path followed by the default robot
  * with that which was desired.
  *
- * @param {list} desired_path A desired path, as printed by RUR.print_path.
+ * @param {list} desired_path A desired path, as printed by `RUR.print_path`.
+ * 
+ * @param {Object} [options] A Javascript object (similar to a Python dict).
  *
- * @returns {bool} True if the correct path was followed.
+ * @param {string} [options.message] If the followed path was not the specified
+ * one and `options.message` is specified, an exception will be raised and
+ * `options.message` will be shown.
+ *
+ * @param {string} [options.show_correct_path] If the followed path was not the specified
+ * one and `options.show_correct_path` evaluates to `true`, the `desired_path`
+ * will be shown. If this is desired, we suggest to use the string `"true"` which
+ * will be valid in both Python and Javascript.
+ * If the correct path is followed, and you wish to show the `desired_path`, 
+ * simply call `RUR.show_correct_path()` explicitly.
+ *
+ * @param {string} [options.color] If the desired path is shown and `options.color`
+ * is specified, it will be the color used to show the path.
+ *
+ * @returns {bool} True if the correct path was followed. If `options.message`
+ * is not specified and the correct path was not followed, it will return false.
  *
  */
 
-RUR.check_path = function (desired_path) {
+RUR.check_path = function (desired_path, options) {
     "use strict";
-    var history, i, world, desired_x, desired_y, path_taken, prev_x, prev_y, x, y;
+    var history, i, world, desired_x, desired_y, path_taken, x, y;
+    var success = true;
+
     world = RUR.get_current_world();
     if (world.robots === undefined || world.robots.length === 0) {
-        throw new RUR.ReeborgError("Missing robot; cannot print path.");
+        throw new RUR.ReeborgError("Missing robot; cannot check path.");
     }
     history = world.robots[0]._trace_history;
-    prev_x = world.robots[0].initial_position[0];
-    prev_y = world.robots[0].initial_position[1];
-    path_taken = [[prev_x, prev_y]];
-    for (i=0; i < history.length; i++) {
-        x = history[i]['grid_x'];
-        y = history[i]['grid_y'];
-        if (x != prev_x || y != prev_y) {
-            path_taken.push([x, y]);
-            prev_x = x;
-            prev_y = y;
-        }
-    }
+    x = world.robots[0].initial_position[0];
+    y = world.robots[0].initial_position[1];
+    path_taken = compute_path(x, y, history);
 
     if (desired_path.length > path_taken.length){
-        console.log("desired_path longer than path taken");
-        return false;
+        success = false;
     } else if (desired_path.length < path_taken.length){
-        console.log("desired_path shorter than path taken");
-        return false;
-    }
-
-    for (i=0; i < path_taken.length; i++) {
-        x = path_taken[i][0];
-        y = path_taken[i][1];
-        desired_x = desired_path[i][0];
-        desired_y = desired_path[i][1];
-        if (x != desired_x || y != desired_y) {
-            console.log("desired = (", desired_x, desired_y, 
-                                    ") ; taken = (", x, y, ")");
-            return false;
+        success = false;
+    } else {
+        for (i=0; i < path_taken.length; i++) {
+            x = path_taken[i][0];
+            y = path_taken[i][1];
+            desired_x = desired_path[i][0];
+            desired_y = desired_path[i][1];
+            if (x != desired_x || y != desired_y) {
+                success = false;
+                break;
+            }
         }
     }
 
-    return true;
+    if (success) {
+        return true;
+    }
+
+    if (options) {
+        if (options.show_correct_path) {
+            if (options.color) {
+                RUR.show_correct_path(desired_path, options.color);
+            } else {
+                RUR.show_correct_path(desired_path);
+            }
+        }
+        if (options.message) {
+            throw new RUR.ReeborgError(options.message);
+        }
+    }
+    return false;
 };
 
 
