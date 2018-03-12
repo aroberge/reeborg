52e67772d62a7c583281008d65aa9ad4be5ac3b0
André Roberge <andre.roberge@gmail.com>
2018-03-12 15:45:15 -0300

    first draft of issue #429 - need to update world selector when changing language

--- src/js/ui/user_progress.js
+++ src/js/ui/user_progress.js
@@ -8,6 +8,7 @@ var remove_fileInput_listener = require("../listeners/onclick.js").remove_fileIn
 
 function update_world_selector (name, remove) {
     var options = $("#select-world")[0].options;
+    console.log("update_world_selector with name, remove:", name, remove);
     for (var i=0; i<options.length; i++) {
         if (remove) {
             if (RUR.strip_checkmark(options[i].innerHTML) == name) {
@@ -28,10 +29,18 @@ RUR.strip_checkmark = function (name) {
 /* Add a checkmark only if the world has been solved.
 */
 RUR.add_checkmark = function (name) {
-    var menu = RUR.state.user_progress[RUR.state.current_menu];
+    var prog_method, menu;
+
     if (name.substring(0,11) === "user_world:"){
         return name;
     }
+
+    prog_method = _get_programming_method();
+    if (RUR.state.user_progress[prog_method] === undefined) {
+        return name;
+    }
+
+    menu = RUR.state.user_progress[prog_method][RUR.state.current_menu];
     if (menu !== undefined && menu.includes(name)) {
         return name += RUR.CHECKMARK;
     }
@@ -39,38 +48,89 @@ RUR.add_checkmark = function (name) {
 };
 
 RUR.update_progress = function(){
-    var world_name, world = RUR.get_current_world();
+    var world_name, prog_method, world = RUR.get_current_world();
+    console.log("entering update_progress");
+    if (RUR.state.input_method == "py-repl") {
+        console.log('input_method', RUR.state.input_method);
+        return;
+    }
     if (world.goal === undefined && world.post === undefined) {
+        console.log("no goal");
         return;   // this world does not have anything that needs to be solved.
     }
     world_name = RUR.state.world_name;
     if (!world_name) {
+        console.log("no world_name", world_name);
         return;
     }
     if (world_name.substring(0,11) === "user_world:"){
+        console.log("user world", world_name);
         return;
     }
-
-    RUR.utils.ensure_key_for_array_exists(RUR.state.user_progress, RUR.state.current_menu);
-    if (!RUR.state.user_progress[RUR.state.current_menu].includes(world_name)) {
-        RUR.state.user_progress[RUR.state.current_menu].push(world_name);
+    console.log("about to update_progress");
+    prog_method = _get_programming_method();
+    RUR.utils.ensure_key_for_obj_exists(RUR.state.user_progress[prog_method], prog_method);
+    RUR.utils.ensure_key_for_array_exists(RUR.state.user_progress[prog_method], RUR.state.current_menu);
+    console.log("prog_method", prog_method);
+    if (!RUR.state.user_progress[prog_method][RUR.state.current_menu].includes(world_name)) {
+        RUR.state.user_progress[prog_method][RUR.state.current_menu].push(world_name);
     }
     update_world_selector(world_name);
     localStorage.setItem("user-progress", JSON.stringify(RUR.state.user_progress));
 };
 
-function retrieve_progress () {
-    var progress = localStorage.getItem("user-progress");
-    if (progress) {
-        try {
-            RUR.state.user_progress = JSON.parse(progress);
-        } catch (e) {
-            RUR.state.user_progress = {};
-        }
+function _get_programming_method() {
+    var programming_method, input_method;
+    input_method = localStorage.getItem("input_method");
+    if (input_method == "blockly-py" || input_method == "blockly-js") {
+        programming_method = "blockly";
+    } else if (input_method == "javascript") {
+        programming_method = "javascript";
+    } else {
+        programming_method = "python";
     }
+    return programming_method;
 }
 
-retrieve_progress();
+/* The first implementation of user progress kept track of world collections (menu)
+   and world names, regardless of the programming method used 
+   (blockly, Python code, Javascript code).
+   The new version, which is introduced just a few months after the intial
+   implementation, allows users to try to solve a given world using different
+   methods, and keep track of progress using each. 
+   When we retrieve the progress status from local storage, we might need
+   to convert from the old implementation to the new one.
+   We guess the conversion based on the current input method which should be
+   the last one used.
+
+ */
+function _retrieve_progress () {
+    var prog_method, progress, user_progress, valid_methods, i, method;
+    valid_methods = ["python", "javascript", "blockly"];
+    progress = localStorage.getItem("user-progress");
+    prog_method = _get_programming_method();
+
+    user_progress = {}
+    if (progress) {
+        try {
+            user_progress = JSON.parse(progress);
+            if (user_progress == null || typeof user_progress == "string") {
+                user_progress = {};
+            }
+        } catch (e) {}
+    }
+
+    for(i=0; i<valid_methods.length; i++){
+        if (user_progress[valid_methods[i]] !== undefined){
+            RUR.state.user_progress = user_progress;  // no conversion needed            
+            return;
+        }
+    }
+    RUR.state.user_progress[prog_method] = user_progress;
+    localStorage.setItem("user-progress", JSON.stringify(RUR.state.user_progress));    
+}
+
+_retrieve_progress();
 
 
 function save_progress() {
@@ -115,12 +175,17 @@ function import_progress () {
 
 function refresh_world_selector() {
     "use strict";
-    var badges, menu, world_name, options = $("#select-world")[0].options;
+    var badges, menu, prog_method, world_name, options = $("#select-world")[0].options;
+    prog_method = _get_programming_method();
+    if (RUR.state.user_progress[prog_method] === undefined) {
+        return;
+    }
     menu = RUR.state.current_menu;
-    badges = RUR.state.user_progress[menu];
+    badges = RUR.state.user_progress[prog_method][menu];
     if (badges === undefined) {
         return;
     }
+
     for (var i=0; i<options.length; i++) {
         world_name = RUR.strip_checkmark(options[i].innerHTML);
         if (badges.includes(world_name)) {
@@ -142,7 +207,10 @@ function refresh_world_selector() {
 
 RUR.unmark_task = function (name) {
     var tasks, remove=true;
-    tasks = RUR.state.user_progress[RUR.state.current_menu];
+    if (RUR.state.user_progress[prog_method] === undefined) {
+        return;
+    }
+    tasks = RUR.state.user_progress[prog_method][RUR.state.current_menu];
     if (tasks === undefined) {
         return;
     }
